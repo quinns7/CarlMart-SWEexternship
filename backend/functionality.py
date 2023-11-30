d85ff883657db630cc25e193f835914e3b967e31
Barinamene Nwike <113485330+BNwike47@users.noreply.github.com>
2023-11-30 08:14:20 -0600

    Webapp Functions
    We now have ways of taking URL arguments and pulling data from the database based off of them.

--- backend/functionality.py
+++ backend/functionality.py
@@ -1,30 +1,44 @@
 import psycopg2
 
 
-conn = psycopg2.connect(dbname="", 
-                            user="tbd", 
-                            host = 'tbd', 
-                            port = 1234, 
-                            password="tbd")
+DB_NAME = "carlsmart"
+DB_USER = "postgres"
+DB_PASS = "carlsarecool"
+DB_HOST = "db"
+DB_PORT = "5432"
 
-#Creates a cursor 
-cur = conn.cursor()
+conn = None
+
+def connect():
+    try:
+        conn = psycopg2.connect(database=DB_NAME,
+                            user=DB_USER,
+                            password=DB_PASS,
+                            host=DB_HOST,
+                            port=DB_PORT)
+    except(Exception, psycopg2.Error) as error:
+                print("Error connecting to PostgreSQL", error) 
+    return
 
 #grabs a specific column list from the specifed table.  
 #Returns a list of tuples
 def select_query(table, column):
+    cur = conn.cursor()
     query = 'SELECT ' + column + 'from ' + table
     cur.execute(query)
     results = cur.fetchall()
+    cur.close()
     return results
 
 #Fetches a row in the chosen table based on a specific column and id
 #Useful if looking for data related to a specifc user, item, etc.
 #Returns a list of tuples
 def select_data(table, column, id):
+    cur = conn.cursor()
     query = 'SELECT * from ' + table + " WHERE " + column + " = " + id
     cur.execute(query) 
     result = cur.fetchall()
+    cur.close()
     return result
 
 #Insert a tuple of data into a SQL table
@@ -32,6 +46,11 @@ def select_data(table, column, id):
 #-Users: (username, listings, rating)
 #-Listings: (listing, title, description, price, contact, image)
 def insert_row(table, data):
+    cur = conn.cursor()
     query = 'INSERT INTO ' + table + ' VALUES ' + data
     cur.execute(query)
+    cur.close()
     return
+
+if __name__ == "__main__":
+    connect()
--- backend/webapp.py
+++ backend/webapp.py
@@ -2,27 +2,61 @@ from flask import Flask, jsonify, request
 import psycopg2
 import functionality
 
+app = Flask(__name__)
 
-app = Flask(__name__, 
-        static_url_path='/static',
-        static_folder='static',
-        template_folder='templates')
+# app = Flask(__name__, 
+#         static_url_path='/static',
+#         static_folder='static',
+#         template_folder='templates')
 
 @app.route('/')
 @app.route('/home')
 def homepage():
     #return render_template('home.html')
-    return
+    return {"home": ["backend info 1", "backend info 2", "backend info 3"]}
 
-@app.route('/item')
+@app.route('/new_item', methods=["POST"])
 def create_item_listing():
+    data = request.get_json()
+    data_list = []
+    for key in data:
+        data_list.append(data[key])
+
+    parsed_data = tuple(data_list)
+    functionality.insert_row("Listings", parsed_data)
+
     #return render_template('home.html')
     return
 
-@app.route('/user')
-def user_profile(username):
+@app.route('/new_user', methods=["POST"])
+def create_item_listing():
+    data = request.get_json()
+    data_list = []
+    for key in data:
+        data_list.append(data[key])
+
+    parsed_data = tuple(data_list)
+    functionality.insert_row("Users", parsed_data)
+
     #return render_template('home.html')
     return
+
+
+#format: .../user?username=bobby
+@app.route('/user')
+def get_user_profile():
+    user_name = request.args.get('username')
+    row = functionality.select_data("Users", "username", user_name)
+    #return render_template('home.html')
+    return row
+
+#Clicking a link to an item should open up that item's page with a url of /item?listing=quinns214532t645325
+@app.route('/item')
+def get_item():
+    item = request.args.get('listing')
+    row = functionality.select_data("Listings", "listing", item)
+    #return render_template('home.html')
+    return row
 
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost')
+    app.run(debug=True, port=8080)
