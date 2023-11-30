b9d9f8c4bf158ddf0d50d64d62be91d6f7d95106
quinns7 <quinns@carleton.edu>
2023-11-30 17:36:00 -0600

    messing with docker and querying the database

--- backend/functionality.py
+++ backend/functionality.py
@@ -11,13 +11,15 @@ conn = None
 
 def connect():
     try:
+        global conn
         conn = psycopg2.connect(database=DB_NAME,
                             user=DB_USER,
                             password=DB_PASS,
                             host=DB_HOST,
                             port=DB_PORT)
         cur = conn.cursor()
-        return cur
+        print("Connected to database!", flush=True)
+        return cur, conn
     except(Exception, psycopg2.Error) as error:
         print("Error connecting to PostgreSQL", error) 
         return 
@@ -25,22 +27,24 @@ def connect():
 #grabs a specific column list from the specifed table.  
 #Returns a list of tuples
 def select_query(table, column):
-    cur = connect()
+    cur, conn = connect()
     query = 'SELECT ' + column + 'from ' + table
     cur.execute(query)
     results = cur.fetchall()
     cur.close()
+    conn.close()
     return results
 
 #Fetches a row in the chosen table based on a specific column and id
 #Useful if looking for data related to a specifc user, item, etc.
 #Returns a list of tuples
 def select_data(table, column, id):
-    cur = connect()
+    cur, conn = connect()
     query = 'SELECT * from ' + table + " WHERE " + column + " = " + id
     cur.execute(query) 
     result = cur.fetchall()
     cur.close()
+    conn.close()
     return result
 
 #Insert a tuple of data into a SQL table
@@ -48,18 +52,20 @@ def select_data(table, column, id):
 #-Users: (username, listings, rating)
 #-Listings: (listing, title, description, price, contact, image)
 def insert_row(table, data):
-    cur = connect()
+    cur, conn = connect()
     query = 'INSERT INTO ' + table + ' VALUES ' + data
     cur.execute(query)
     cur.close()
+    conn.close()
     return
 
 def select_all_listings():
-    cur = connect()
-    query = 'SELECT * from listings'
+    cur, conn = connect()
+    query = 'SELECT * FROM users;'
     cur.execute(query) 
     result = cur.fetchall()
     cur.close()
+    conn.close()
     return result
 
 if __name__ == "__main__":
