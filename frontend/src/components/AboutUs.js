dc385e37dc30796dfca0a3aae1228c2c3b315828
Hannah P Moran <95652541+moranhannah@users.noreply.github.com>
2023-12-08 15:31:19 -0600

    about us page completed (edit your own blurb as needed!)

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -13,7 +13,7 @@ function AboutUs() {
   const [showCategories, setShowCategories] = useState(false);
   const categories = [{value: 'books', label: 'Books'}, {value: 'electronics', label: 'Electronics'}, {value: 'apparel', label: 'Apparel'}, {value: 'furniture', label:'Furniture'}, {value: 'toys', label: 'Toys'}];
   const navigate = useNavigate();
-  const devInfo = [['Barry', 'CS student at Carleton'], ['Hannah', 'Another CS student at Carleton'], ['Sophie','Yet another CS student at Carleton'], ['Zoey','And another CS student at Carleton']];
+  const devInfo = [['Barry Nwike', 'Barinamene "Barry" Nwike is a junior CS major/Creative Writing minor at Carleton College.   When he\'s not programming, he likes taking long walks, cooking, and writing.'], ['Hannah Moran', 'Hannah is a senior Computer Science major, Digital Arts and Humanities minor at Carleton College. She is a member of the Carleton and Chamber Choirs and Carleton\'s Hack4Impact club. In her free time, she enjoys running and warm coffee.'], ['Sophie Quinn','Sophie is a member of the class of \’25, majoring in Computer Science, and minoring in History and German. She is involved with the Nova (ultimate frisbee team), JCO (student dance group), and FKK (german club).'], ['Zoey La','Hi there! My name is Zoey — I’m currently a junior at Carleton, majoring in CS and minoring in Math! In my free time, I play Fortnite. And I also (attempt to) code, I guess.']];
   //const devInfo = ['CS student at Carleton', 'Another CS student at Carleton', 'Yet another CS student at Carleton', 'And another CS student at Carleton'];
 
   const handleNavToNewListing = () => {
@@ -73,7 +73,8 @@ function AboutUs() {
       </header>
 
       <section className="about-us" style={aboutUsStyle}>
-        <h2 style={{ marginLeft: '-20px', marginRight: '-20px' }}>About Us</h2> 
+        <h2 style={{ marginLeft: '-20px', marginRight: '-20px', fontWeight: 'bold', fontSize: '30px' }}>About Us</h2> 
+        <h2 style={{ marginLeft: '-20px', marginRight: '-20px', fontSize: '20px' }}>Welcome to CarlMart, a collaborative effort by a talented team of developers, mentored by Carleton alumni Ethan Cassel-Mace over two weeks. Our structured web application simplifies the buying and selling of new or used items on campus, creating a convenient and community-driven marketplace for students. The following students developed CarlMart during the Winter 2023 SWE Externship program.</h2> 
         <div className="about-us-grid">
           {devInfo.map((info, i) => (
             <div key={i} className="devInfo-card">
