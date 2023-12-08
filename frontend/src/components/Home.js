e007defcd386cc2b3dcec58c2988d5279bf62d2c
Zoey La <zoeyla24@gmail.com>
2023-12-09 02:13:45 +0700

    fixed big ass tile

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -44,7 +44,9 @@ function Home() {
   const [selectedListing, setSelectedListing] = useState(null);
   const navigate = useNavigate();
   const [sort, setSort] = useState('');
-  const [data, setData] = useState([{}])
+  // const [data, setData] = useState([{}])
+  const [data, setData] = useState({ home: [] }); // Initialize 'home' as an empty array
+
 
   useEffect(() => {
     fetch("/home").then(
@@ -219,48 +221,37 @@ function Home() {
         </div>
 
         <div className="new-listings-grid">
-          {/* Display default items if no search is performed */}
-          {!isSearching && (
-            <div>
-              {typeof data.home === 'undefined' ? (
-                <p>Loading...</p>
-              ) : (
-                data.home.map((unit, i) => (
-                  <div key={i} className="listing-card" onClick={() => openModal(unit)}>
-                    <img
-                      src={`https://res.cloudinary.com/dpsysttyv/image/upload/w_200,h_100,c_fill,q_100/${unit[4]}.jpg`}
-                      alt={unit[0]}
-                      className="listing-image"
-                    />
-                    <div className="listing-details">
-                      <h3 className="listing-title">{unit[0]}</h3>
-                      <p className="listing-price">${unit[2]}</p>
-                    </div>
-                  </div>
-                ))
-              )}
-            </div>
-          )}
-
-          {/* Display search results after a search is performed */}
-          {isSearching && (
-            <div>
-              {searchResults.map((item, index) => (
-                <div key={index} className="listing-card" onClick={() => openModal(item)}>
-                  <img
-                    src={`https://res.cloudinary.com/dpsysttyv/image/upload/w_200,h_100,c_fill,q_100/${item[5]}.jpg`}
-                    alt={item[1]}
-                    className="listing-image"
-                  />
-                  <div className="listing-details">
-                    <h3 className="listing-title">{item[1]}</h3>
-                    <p className="listing-price">${item[3]}</p>
-                  </div>
+          {!isSearching ? (
+            data.home.map((unit, i) => (
+              <div key={i} className="listing-card" onClick={() => openModal(unit)}>
+                <img
+                  src={`https://res.cloudinary.com/dpsysttyv/image/upload/w_200,h_100,c_fill,q_100/${unit[4]}.jpg`}
+                  alt={unit[0]}
+                  className="listing-image"
+                />
+                <div className="listing-details">
+                  <h3 className="listing-title">{unit[0]}</h3>
+                  <p className="listing-price">${unit[2]}</p>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))
+          ) : (
+            searchResults.map((item, index) => (
+              <div key={index} className="listing-card" onClick={() => openModal(item)}>
+                <img
+                  src={`https://res.cloudinary.com/dpsysttyv/image/upload/w_200,h_100,c_fill,q_100/${item[5]}.jpg`}
+                  alt={item[1]}
+                  className="listing-image"
+                />
+                <div className="listing-details">
+                  <h3 className="listing-title">{item[1]}</h3>
+                  <p className="listing-price">${item[3]}</p>
+                </div>
+              </div>
+            ))
           )}
         </div>
+
       </section>
 
       
